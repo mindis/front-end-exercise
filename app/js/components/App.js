218e1e63a4b97a8263b9d779dc2852847de8e4e0
Anthony Fader <afader@gmail.com>
2015-10-02 15:20:46 -0700

    masonry-based app done

--- app/js/components/App.js
+++ app/js/components/App.js
@@ -1,11 +1,38 @@
 var React = require('react');
+var PostGrid = require('./PostGrid.js');
+var Spinner = require('./Spinner.js');
+var Panel = require('react-bootstrap').Panel;
+var posts = require('../posts.js');
 var App = React.createClass({
+  getInitialState: function() {
+    return { loading: true, error: null, posts: null };
+  },
+  updatePosts: function(data) {
+    this.setState({ loading: false, posts: data, error: null });
+  },
+  setError: function(err) {
+    this.setState({ loading: false, posts: null, error: err });
+  },
+  componentWillMount: function() {
+    posts.get(this.updatePosts, this.setError);
+  },
+  renderLoading: function() {
+    return <div className="middle"><Spinner width={50} height={50}/></div>;
+  },
+  renderError: function() {
+    return <Panel header="Error" bsStyle="danger">{this.state.error}</Panel>;
+  },
+  renderPosts: function() {
+    return <PostGrid posts={this.state.posts}/>;
+  },
   render: function() {
-    return (
-      <div>
-	<h1>Hello, world</h1>
-      </div>
-    );
+    if (this.state.loading) {
+      return this.renderLoading();
+    } else if (this.state.error) {
+      return this.renderError();
+    } else {
+      return this.renderPosts();
+    }
   }
 });
 module.exports = App;
---
+++ app/js/config.js
@@ -0,0 +1,3 @@
+var config = {};
+config.url = 'https://appsheettest1.azurewebsites.net/sample/posts';
+module.exports = config;
