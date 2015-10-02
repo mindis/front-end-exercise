91be877b4f2f04fcb6927275acad9d7f905e7cae
Anthony Fader <afader@gmail.com>
2015-10-02 15:21:31 -0700

    Update 2015-10-02T22:21:27.724Z

--- README.md
+++
@@ -1,2 +0,0 @@
-# front-end-exercise
-a front-end programming exercise: display images in a fluid layout
--- app/js/config.js
+++
@@ -1,3 +0,0 @@
-var config = {};
-config.url = 'https://appsheettest1.azurewebsites.net/sample/posts';
-module.exports = config;
